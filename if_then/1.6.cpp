da0c8ed8c36874894662beda4728c73b347a5ae5
hathanh94vn <bcs_condom_online@yahoo.com>
2014-02-13 20:02:40 +0700

    Revert "go gi cung dc, khi hien cai nay, co nghia la co thay doi ve file :)"
    This reverts commit 44c7cd41bdb60d93b6ce25c8794220b7eec38550.

--- if_then/1.6.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream.h>
-#include<math.h>
-main(void)
-{
-	float a=2.5,b=1.5,c=4,x;
-	cout<<"nhap x\n";
-	cin>>x;
-	cout<<"\ny= "<<(pow(x,3)+sin(b)-exp(0.234))/(5+exp(b)+c*abs(0.25+x)-sqrt(x+cos(b)));
-}
